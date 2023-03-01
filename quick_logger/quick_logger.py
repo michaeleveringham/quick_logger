ee5ecad5611a641de743a9829f87b360e3b73907
LamerLink <36551116+LamerLink@users.noreply.github.com>
2023-02-28 23:56:29 -0500

    Make file handler optional, add stream handler option.

--- quick_logger/quick_logger.py
+++ quick_logger/quick_logger.py
@@ -1,37 +1,43 @@
 import logging
 
 
+LOGGER_LEVELS = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG,
+    'notset': logging.NOTSET
+}
+
+
 def init_logger(
-    file_path: str,
+    file_path: str = None,
+    stream: bool = True,
     level: str = 'info',
-    fmt: str = '',
-    datefmt: str = ''
+    fmt: str = "%(asctime)s:: %(levelname)s:: %(message)s",
+    datefmt: str = "%Y-%m-%d %H:%M:%S"
 ) -> None:
-    levels = {
-        'critical': logging.CRITICAL,
-        'error': logging.ERROR,
-        'warning': logging.WARNING,
-        'info': logging.INFO,
-        'debug': logging.DEBUG,
-        'notset': logging.NOTSET
-    }
+    if not stream and not file_path:
+        raise ValueError("Must supply at least one of file_path or stream.")
     try:
-        level = levels[level]
+        level = LOGGER_LEVELS[level]
     except KeyError:
-        print(f'Invalid level selection of {level}, using "info".')
-        level = levels['info']
-    handler = logging.FileHandler(file_path)
-    if not fmt:
-        fmt = '%(asctime)s:: %(levelname)s:: %(message)s'
-    if not datefmt:
-        datefmt = '%Y-%m-%d %H:%M:%S'
+        print(f'Invalid level selection of {level}, defaulting to "info".')
+        level = LOGGER_LEVELS['info']
     formatter = logging.Formatter(fmt, datefmt)
-    handler.setFormatter(formatter)
-    handler.setLevel(level)
-    #handler.setStream(stream = None)
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
-    logger.addHandler(handler)
+    handlers = []
+    if file_path:
+        file_handler = logging.FileHandler(file_path)
+        file_handler.setStream(stream=None)
+        handlers.append(file_handler)
+    if stream:
+        handlers.append(logging.StreamHandler())
+    for handler in handlers:
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
 def mlog(message: str, level: str = 'info') -> None:
     logger = logging.getLogger(__name__)
